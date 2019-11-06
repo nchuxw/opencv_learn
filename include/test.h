f88f7b8eced5fd8d048f9ba5aa0ac71787f614a0
nchuxxx <16201325.stu.nchu.edu.cn>
2019-11-06 17:42:44 +0800

    first commit

---
+++ include/test.h
@@ -0,0 +1,14 @@
+#ifndef __TEST_H
+#define __TEST_H
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+#endif
