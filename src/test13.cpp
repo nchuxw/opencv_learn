11c97564bc84460a50b1a4c7b71697c45e2ebed6
nchuxw <1601325@stu.nchu.edu.cn>
2020-03-10 18:18:53 +0800

    双线性内插效果不好，应该是哪里有问题

---
+++ src/test13.cpp
@@ -0,0 +1,83 @@
+/* 图片的缩放 */
+#include "test.h"
+
+/* 最近邻内插 */
+void nearest_interpolation(Mat src, Mat &dst, Size dst_size)
+{
+	int i, j, x, y;
+	/* 计算原图像与目标图像的宽度和高度比例 */
+	double wsd = (double)src.cols / dst_size.width;
+	double hsd = (double)src.rows / dst_size.height;
+
+	dst = Mat(dst_size, src.type());
+	for(i = 0; i < dst.rows; i++)
+	{
+		y = hsd * i + 0.5;
+		for(j = 0; j < dst.cols; j++)
+		{
+			x = wsd * j + 0.5;
+			dst.at<uchar>(i, j) = src.at<uchar>(y, x);
+		}
+	}
+}
+
+/* 双线性内插 */
+void bilinear_interpolation(Mat src, Mat &dst, Size dst_size)
+{
+	int i, j;
+	/* 计算原图像与目标图像的宽度和高度比例 */
+	double wsd = (double)src.cols / dst_size.width;
+	double hsd = (double)src.rows / dst_size.height;
+	/* 表示原图中的浮点像素 */
+	Point2f sfp;
+	/* 表示在原图中与浮点像素最近的四个整数坐标值(左x、右x、上y、下y) */
+	double lx, rx, uy, dy;
+	/* x方向的两个像素点值，xy两个方向上的像素点值 */
+	double xf1, xf2, xyf;
+
+	dst = Mat(dst_size, src.type());
+	for(i = 0; i < dst.rows; i++)
+	{
+		sfp.y = hsd * i;
+		for(j = 0; j < dst.cols; j++)
+		{
+			sfp.x = wsd * j;
+			lx = (int)sfp.x;
+			rx = lx + 1;
+			uy = (int)sfp.y;
+			dy = uy + 1;
+
+			xf1 = (sfp.x - lx) * src.at<uchar>(uy, lx) + (rx - sfp.x) * src.at<uchar>(uy, rx);
+			xf2 = (sfp.x - lx) * src.at<uchar>(dy, lx) + (rx - sfp.x) * src.at<uchar>(dy, rx);
+			xyf = (sfp.y - uy) * xf1 + (dy - sfp.y) * xf2;
+			dst.at<uchar>(i, j) = (uchar)xyf;
+		}
+	}
+}
+
+int main()
+{
+	Mat src, dst;
+	Size dst_size;
+
+	src = cv::imread("img/img1.png", cv::IMREAD_COLOR);
+	cvtColor(src, src, COLOR_BGR2GRAY);
+	imshow("src", src);
+	waitKey(100);
+
+	while(scanf("%d %d", &dst_size.height, &dst_size.width) != EOF)
+	{
+		cv::resize(src, dst, dst_size, INTER_LINEAR);
+		imshow("opencv resize", dst);
+		waitKey(100);
+
+		nearest_interpolation(src, dst, dst_size);
+		imshow("nearest interpolation", dst);
+		waitKey(100);
+
+		bilinear_interpolation(src, dst, dst_size);
+		imshow("bilinear interpolation", dst);
+		waitKey(100);
+	}
+	return 0;
+}
