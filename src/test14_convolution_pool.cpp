7e723422d4f63bc27e6fb37d6794418157733ae2
nchuxw <1601325@stu.nchu.edu.cn>
2020-06-29 16:42:17 +0800

    update convolution pool

--- src/test14_convolution_pool.cpp
+++ src/test14_convolution_pool.cpp
@@ -2,34 +2,66 @@
 #include "test.h"
 #define MAX(a, b) ((a) < (b) ? (b) : (a))
 
-int main()
+void pool_max(Mat src, Mat &des)
 {
 	int i, j;
-	Mat img = imread("img/img1.png", cv::IMREAD_COLOR);
-	imshow("src", img);
 
-	Mat des = Mat(img.rows, img.cols, img.type());
-	Mat kernel = (Mat_<char>(3, 3) << 2, -1, 2, -1, -4, -1, 2, -1, 2);
-	filter2D(img, des, img.depth(), kernel);
-	cvtColor(des, des, cv::COLOR_BGR2GRAY);
-	imshow("des_con", des);
-	// imwrite("./img/des_con.png", des);
-
-	Mat des2 = Mat(des.rows / 2, des.cols / 2, des.type());
-	for(i = 0; i < des2.rows; i++)
+	for(i = 0; i < des.rows; i++)
 	{
-		for(j = 0; j < des2.cols; j++)
+		for(j = 0; j < des.cols; j++)
 		{
 			uchar max = 0;
-			max = MAX(max, des.at<uchar>(i * 2, j * 2));
-			max = MAX(max, des.at<uchar>(i * 2 + 1, j * 2));
-			max = MAX(max, des.at<uchar>(i * 2, j * 2 + 1));
-			max = MAX(max, des.at<uchar>(i * 2 + 1, j * 2 + 1));
-			des2.at<uchar>(i, j) = max;
+			max = MAX(max, src.at<uchar>(i * 2, j * 2));
+			max = MAX(max, src.at<uchar>(i * 2 + 1, j * 2));
+			max = MAX(max, src.at<uchar>(i * 2, j * 2 + 1));
+			max = MAX(max, src.at<uchar>(i * 2 + 1, j * 2 + 1));
+			des.at<uchar>(i, j) = max;
 		}
 	}
-	imshow("des_pool", des2);
+}
+
+void pool_ave(Mat src, Mat &des)
+{
+	int i, j;
+
+	for(i = 0; i < des.rows; i++)
+	{
+		for(j = 0; j < des.cols; j++)
+		{
+			int ave = 0;
+			ave += src.at<uchar>(i * 2, j * 2);
+			ave += src.at<uchar>(i * 2 + 1, j * 2);
+			ave += src.at<uchar>(i * 2, j * 2 + 1);
+			ave += src.at<uchar>(i * 2 + 1, j * 2 + 1);
+			des.at<uchar>(i, j) = (uchar)(ave / 4);
+		}
+	}
+}
+
+int main()
+{
+	Mat img = imread("img/img1.png", cv::IMREAD_GRAYSCALE);
+	imshow("src", img);
+
+	Mat kernel1 = (Mat_<char>(3, 3) << 2, -1, 2, -1, -4, -1, 2, -1, 2);
+	Mat des1 = Mat(img.rows, img.cols, img.type());
+	filter2D(img, des1, img.depth(), kernel1);
+	imshow("des1_con", des1);
+	// imwrite("./img/des_con.png", des);
+
+	Mat des2 = Mat(des1.rows / 2, des1.cols / 2, des1.type());
+	pool_ave(des1, des2);
+	imshow("des2_pool", des2);
 	// imwrite("./img/des_pool.png", des2);
+
+	Mat kernel2 = (Mat_<char>(3, 3) << 0, -1, 0, -1, 4, -1, 0, -1, 0);
+	Mat des3 = Mat(des2.rows, des2.cols, des2.type());
+	filter2D(des2, des3, des2.depth(), kernel2);
+	imshow("des3_con", des3);
+
+	Mat des4 = Mat(des3.rows / 2, des3.cols / 2, des3.type());
+	pool_ave(des3, des4);
+	imshow("des4_pool", des4);
 
 	waitKey(0);
 	return 0;
