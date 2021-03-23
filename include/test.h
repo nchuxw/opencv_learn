ef980e31687a85d593b07de41db4a400e08b4fae
stxw <stxws@qq.com>
2021-03-23 16:01:53 +0800

    add lbp_feature

--- include/test.h
+++ include/test.h
@@ -2,13 +2,9 @@
 #define __TEST_H
 
 #include <stdio.h>
-#include <iostream>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
-using namespace std;
-using namespace cv;
 
 #endif
